5aca3cd34f4961462736daa4c66cdc3ac33abe90
212556286 <srivaisnave.parthiban@ge.com>
2017-12-15 22:17:12 +0530

    code change

--- src/test/TestJava.java
+++ src/test/TestJava.java
@@ -3,12 +3,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "test")
+@SpringBootApplication
 public class TestJava {
 
 	public static void main(String[] args) {
@@ -22,7 +24,7 @@ public class TestJava {
 
 @RestController
 class HomeController {
-	@RequestMapping("/testAPI")
+	@RequestMapping("hello")
 	String hello() {
 		return "Hiii";
 	}

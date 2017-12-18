b5c6b3513fd1cd18a246330b4ceb60e9e19fb493
212556286 <srivaisnave.parthiban@ge.com>
2017-12-18 12:57:46 +0530

    code change

--- src/test/TestJava.java
+++ src/test/TestJava.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages="test")
 @EnableAutoConfiguration
 @SpringBootApplication
 public class TestJava extends SpringBootServletInitializer {

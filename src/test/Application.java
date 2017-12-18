c549b93b311a35739b89441040749edf9c95c972
212556286 <srivaisnave.parthiban@ge.com>
2017-12-18 14:54:32 +0530

    code updated

--- src/test/Application.java
+++ src/test/Application.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages="test")
+@ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {

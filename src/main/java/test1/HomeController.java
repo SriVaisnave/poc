e489991deebe40d374accf66d2d4e28ef80f2383
212556286 <srivaisnave.parthiban@ge.com>
2017-12-20 14:21:25 +0530

    code change

--- src/main/java/test1/HomeController.java
+++ src/main/java/test1/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class HomeController {
+public class HomeController {
 	@RequestMapping("/")
 	public String hello() {
 		return "Hiii";
